d5c0eacb7eb128edd9f22d53e85c8e1f158a39a5
Sokmean Nou <sokmean.nou@gmail.com>
2017-01-31 11:17:07 -0500

    static UI

--- starting-point/public/scripts/gmaps.js
+++ starting-point/public/scripts/gmaps.js
@@ -1,16 +1,96 @@
 // Google Maps API
-
 function initialize_gmaps() {
     // initialize new google maps LatLng object
     var myLatlng = new google.maps.LatLng(40.705189,-74.009209);
     // set the map options hash
     var mapOptions = {
         center: myLatlng,
-        zoom: 16,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
+        zoom: 15,
+        scrollwheel: false,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        styles: [
+            {elementType: 'geometry', stylers: [{color: '#ffffff'}]},
+            {elementType: 'labels.text.stroke', stylers: [{color: '#ffffff'}]},
+            {elementType: 'labels.text.fill', stylers: [{color: '#000000'}]},
+            {
+              featureType: 'administrative.locality',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#000000'}]
+            },
+            {
+              featureType: 'poi',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#000000'}]
+            },
+            {
+              featureType: 'poi.park',
+              elementType: 'geometry',
+              stylers: [{color: '#accc9b'}]
+            },
+            {
+              featureType: 'poi.park',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#000000'}]
+            },
+            {
+              featureType: 'road',
+              elementType: 'geometry',
+              stylers: [{color: '#c7c9cc'}]
+            },
+            {
+              featureType: 'road',
+              elementType: 'geometry.stroke',
+              stylers: [{color: '#c7c9cc'}]
+            },
+            {
+              featureType: 'road',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#000000'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'geometry',
+              stylers: [{color: '#c7c9cc'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'geometry.stroke',
+              stylers: [{color: '#505050'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#000000'}]
+            },
+            {
+              featureType: 'transit',
+              elementType: 'geometry',
+              stylers: [{color: '#5b3d35'}]
+            },
+            {
+              featureType: 'transit.station',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#black'}]
+            },
+            {
+              featureType: 'water',
+              elementType: 'geometry',
+              stylers: [{color: '#F0F0F0'}]
+            },
+            {
+              featureType: 'water',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#000000'}]
+            },
+            {
+              featureType: 'water',
+              elementType: 'labels.text.stroke',
+              stylers: [{color: '#F0F0F0'}]
+            }
+          ]
     };
     // get the maps div's HTML obj
-    var map_canvas_obj = document.getElementById("map");
+    var map_canvas_obj = document.getElementById('map');
     // initialize a new Google Map with the options
     var map = new google.maps.Map(map_canvas_obj, mapOptions);
     // Add the marker to the map
@@ -23,5 +103,6 @@ function initialize_gmaps() {
 }
 
 $(document).ready(function() {
+    console.log("ready");
     initialize_gmaps();
 });
---
+++ starting-point/public/scripts/jquery.js
@@ -0,0 +1,12 @@
+// const $ = require('jquery');
+
+// $(document).ready(function(){
+//     $(window).scroll(function(){
+//         if ($(this).scrollTop() > 200) {
+//             $('#menu').fadeIn(500);
+//         } else {
+//             $('#menu').fadeOut(500);
+//         }
+//     });
+// });
+
